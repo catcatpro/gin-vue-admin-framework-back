0ba5303088dfd359e33d19b566ed6eb01bc12679
catcatproer <1901749687@qq.com>
2024-07-29 15:49:23 +0800

    添加example路由

--- internal/routes/example.go
+++ internal/routes/example.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	"gin_vue_admin_framework/internal/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var exampleController *controllers.ExampleController = new(controllers.ExampleController)
+
+func LoadExampleRoutes(r *gin.Engine) *gin.RouterGroup {
+	exampleGroup := r.Group("/example")
+	{
+		exampleGroup.GET("/index", exampleController.IndexAction)
+	}
+	return exampleGroup
+}
