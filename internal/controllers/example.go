e63eb02ddebc5a180aecb62ea725e03ef4464aca
catcatproer <1901749687@qq.com>
2024-07-29 15:48:25 +0800

    添加example控制器

--- internal/controllers/example.go
+++ internal/controllers/example.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ExampleController struct{}
+
+func (ExampleController *ExampleController) IndexAction(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"hello": "world",
+	})
+}
