ce101f1d36bcf29b4c15f09f7a33920d6c1b01a3
catcatproer <1901749687@qq.com>
2024-07-29 15:49:51 +0800

    添加example路由

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,1 +1,14 @@
-package router
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var Router *gin.Engine
+
+func Init() {
+	Router = gin.Default()
+
+	//加载路由
+	LoadExampleRoutes(Router)
+}
