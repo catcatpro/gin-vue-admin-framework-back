81612b977f4dda5c7eb24e6e54f6dc708613a868
catcatproer <1901749687@qq.com>
2024-08-01 17:28:15 +0800

    添加测试session方法

--- internal/controllers/example.go
+++ internal/controllers/example.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -10,5 +11,14 @@ type ExampleController struct{}
 func (ExampleController *ExampleController) IndexAction(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"hello": "world",
+	})
+}
+
+func (ExampleController *ExampleController) TestSessionAction(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+	session.Set("test", "test_value")
+	session.Save()
+	ctx.JSON(http.StatusOK, gin.H{
+		"test": session.Get("test"),
 	})
 }
