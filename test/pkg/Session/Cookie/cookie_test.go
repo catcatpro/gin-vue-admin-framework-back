420764787e5b16bfd0b3b4a1aab8e4c892dd66a7
catcatproer <1901749687@qq.com>
2024-08-01 17:39:13 +0800

    修改session的测试用例

--- test/pkg/Session/Cookie/cookie_test.go
+++ test/pkg/Session/Cookie/cookie_test.go
@@ -1,14 +1,34 @@
 package Session
 
 import (
-	"gin_vue_admin_framework/configs"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"gin_vue_admin_framework/internal/routes"
+	"github.com/go-playground/assert/v2"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
-func initSession(r *gin.Engine) {
-	sessionConfig := configs.SystemConfigs.Session
-	store := cookie.NewStore([]byte(sessionConfig.Secret))
-	r.Use(sessions.Sessions("systemSession", store))
+type session struct {
+	Test string `json:"test"`
+}
+
+func TestSessionCookie(t *testing.T) {
+	var w *httptest.ResponseRecorder
+	r := routes.Router
+	url := "/example/test_session"
+	req, _ := http.NewRequest("GET", url, nil)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	m := session{}
+	body, _ := io.ReadAll(w.Body)
+	err := json.Unmarshal(body, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, m.Test, "test_value")
 }
