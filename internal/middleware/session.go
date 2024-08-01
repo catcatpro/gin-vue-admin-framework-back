0f12a9a05a2591660e44c92d9314aa53782aa8ce
catcatproer <1901749687@qq.com>
2024-08-01 17:30:57 +0800

    添加session中间件

--- internal/middleware/session.go
+++ internal/middleware/session.go
@@ -1,1 +1,12 @@
 package middleware
+
+import (
+	Session "gin_vue_admin_framework/pkg/Session/Cookie"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func HandleSession(r *gin.Engine) {
+	store := Session.InitSession()
+	r.Use(sessions.Sessions("systemSession", store))
+}
