28f8914b734a6287b2cdf69d82cc207d4e208751
catcatproer <1901749687@qq.com>
2025-05-09 18:31:38 +0800

    删除登录接口

--- internal/controllers/SystemController.go
+++ internal/controllers/SystemController.go
@@ -1,9 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-	"gin_vue_admin_framework/internal/models/requests"
-
 	"gin_vue_admin_framework/internal/services"
 	"net/http"
 
@@ -11,40 +8,6 @@ import (
 )
 
 type SystemController struct{}
-
-func (sc *SystemController) SysLoginAction(c *gin.Context) {
-	loginInfo := requests.SysLoginRequest{}
-	err := c.ShouldBind(&loginInfo)
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-			"status": "error",
-			"msg":    "Parameter error",
-			"data":   "{}",
-		})
-
-		return
-	}
-	fmt.Println(loginInfo)
-	cs := services.SysUserService{}
-	token, err := cs.Login(&loginInfo)
-
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"status": "error",
-			"msg":    err.Error(),
-			"data":   "{}",
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-		"msg":    "success",
-		"data": gin.H{
-			"token": token,
-		},
-	})
-
-}
 
 func (sc *SystemController) GetCaptchaAction(c *gin.Context) {
 	ss := services.SysService{}
