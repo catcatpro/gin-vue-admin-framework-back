c4a09363d78bf39b8f68c3208f6fde7b346142e0
catcatproer <1901749687@qq.com>
2025-05-09 18:50:14 +0800

    将登录接口迁移到此

--- internal/routes/admin/user.go
+++ internal/routes/admin/user.go
@@ -1,3 +1,17 @@
-package routes
+package AdminRoutes
 
-type UserRouter struct{}
+import (
+	adminControllers "gin_vue_admin_framework/internal/controllers/admin"
+	"github.com/gin-gonic/gin"
+)
+
+type AdminUserRouter struct{}
+
+var adminUserController = new(adminControllers.UserController)
+
+func (ur *AdminUserRouter) InitRouter(PublicRouter *gin.RouterGroup, PrivateRouter *gin.RouterGroup) {
+	adminUserGroup := PublicRouter.Group("admin_user")
+	{
+		adminUserGroup.POST("login", adminUserController.SysLoginAction)
+	}
+}
