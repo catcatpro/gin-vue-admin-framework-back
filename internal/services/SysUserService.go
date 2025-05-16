e41c9ab30ab817817427d88e8571c2eec1bc4423
catcatproer <1901749687@qq.com>
2025-05-16 18:11:41 +0800

    新增添加用户service

--- internal/services/SysUserService.go
+++ internal/services/SysUserService.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"errors"
-	"fmt"
 	"gin_vue_admin_framework/internal/models"
 	"gin_vue_admin_framework/internal/models/requests"
 	"gin_vue_admin_framework/utils"
@@ -11,10 +10,9 @@ import (
 
 type SysUserService struct{}
 
-func (cs *SysUserService) Login(UserReq *requests.SysLoginRequest) (string, error) {
+func (cs *SysUserService) Login(UserReq *requests.LoginRequest) (string, error) {
 	var cap utils.CaptchaInterfaceV2
 	cap = new(utils.CaptchaV2)
-	fmt.Println("mode2", gin.Mode())
 	//验证码验证
 	if res, err := cap.Verify(UserReq.CaptchaId, UserReq.Captcha); (err != nil || !res) && gin.Mode() == gin.ReleaseMode {
 
@@ -26,18 +24,34 @@ func (cs *SysUserService) Login(UserReq *requests.SysLoginRequest) (string, erro
 	userInfo.Password = UserReq.Password
 
 	//执行登录验证
-	if !userInfo.UserLogin() {
+	if err := userInfo.UserLogin(); err != nil {
 		return "", errors.New("Username or Password incorrect")
 	}
 
 	j := utils.NewJWT()
 
-	//TODO userInfo.ID
-	claims := j.CreateClaims(1, userInfo.Username)
+	claims := j.CreateClaims(userInfo.ID, userInfo.Username)
 	token, err := j.CreateToken(claims)
 
 	if err != nil {
 		return "", err
 	}
 	return token, nil
+}
+
+func (cs *SysUserService) CreateUser(req *requests.CreateUserRequest) error {
+
+	//确认密码是否正确
+	if req.ConfirmPassword != req.Password {
+		return errors.New("confirm password not match")
+	}
+
+	var userInfo models.User
+	userInfo.Username = req.Username
+	userInfo.Password = req.Password
+	if _, err := userInfo.CreateUser(); err != nil {
+		return err
+	}
+
+	return nil
 }
