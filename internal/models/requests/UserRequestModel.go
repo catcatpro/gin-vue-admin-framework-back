30a99f04cc2a06e4e0d95e6ec61248b2257bd163
catcatproer <1901749687@qq.com>
2025-05-16 18:10:13 +0800

    新增添加用户表单模型

--- internal/models/requests/UserRequestModel.go
+++ internal/models/requests/UserRequestModel.go
@@ -1,8 +1,16 @@
 package requests
 
+type UserRequest struct {
+	Username string `json:"username" form:"username" binding:"required"`
+	Password string `json:"password" form:"password" binding:"required"`
+}
 type LoginRequest struct {
-	Username  string `json:"username" form:"username" binding:"required"`
-	Password  string `json:"password" form:"password" binding:"required"`
+	UserRequest
 	Captcha   string `json:"captcha" form:"captcha" binding:"required"`
 	CaptchaId string `json:"captcha_id" form:"captcha_id" binding:"required"`
+}
+
+type CreateUserRequest struct {
+	UserRequest
+	ConfirmPassword string `json:"confirm_password" form:"confirm_password" binding:"required"`
 }
